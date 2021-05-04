3c3e569c672f55b204c54c5664ac96217a83c241
ashutosh-aanand <ashutosh.aanand@gmail.com>
2021-05-04 19:14:20 +0530

    styled input form

--- src/comps/Upload.js
+++ src/comps/Upload.js
@@ -22,7 +22,10 @@ const Upload = () => {
 
     return (
         <form>
-            <input type="file" onChange={handleChange} />
+            <label>
+                <input type="file" onChange={handleChange} />
+                <span>+</span>
+            </label>
             <div className="output">
                 { error && <div className="error">{ error }</div> }
                 { file && <div> { file.name }</div> }
