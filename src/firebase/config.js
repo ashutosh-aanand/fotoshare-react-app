fea6f1655711e38f1cdf3f6b19e8a5248cf3265b
ashutosh-aanand <ashutosh.aanand@gmail.com>
2021-05-04 20:30:11 +0530

    t0

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,9 +4,9 @@ import firebase from 'firebase/app'
 import 'firebase/storage'
 import 'firebase/firestore'
 
-if(process.env.NODE_ENV !== "production"){
-    require('dotenv').config();
-}
+
+require('dotenv').config();
+
 
 console.log(process.env.apiKey)
 
