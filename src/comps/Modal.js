7daa7db9d048bd07fe3072c2e486e818381bb6da
ashutosh-aanand <ashutosh.aanand@gmail.com>
2021-05-04 18:09:45 +0530

    backdrop and image differentiation

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,8 +1,10 @@
 const Modal = ({ selImgUrl, setSelImgUrl }) => {
 
-    // to unmount Modal when the backdrop is clicked
+    // to unmount Modal when the backdrop is clicked (not the image)
     const handleClick = (e) => {
         // console.log(e.target);
+        
+        // to differentiate image and backdrop
         if(e.target.classList.contains('backdrop')){
             setSelImgUrl(null);
             // when selImgUrl is null, Modal won't be displayed (see in App component)
