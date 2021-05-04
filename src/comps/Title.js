cc1a330bd38c5af959b9a0281ec351532f395786
ashutosh-aanand <ashutosh.aanand@gmail.com>
2021-05-04 19:15:20 +0530

    changed text

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ const Title = () => {
         <div className="title">
             <h1>fotoshare</h1>
             <h2>your fotos</h2>
-            <p>nice to see you here !</p>
+            <p>share your fotos with the world !</p>
         </div>
     );
 }
