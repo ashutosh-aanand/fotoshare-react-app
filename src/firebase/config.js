61195cdfd98d1b3dd18f2d5c0252fc795c0510f8
ashutosh-aanand <ashutosh.aanand@gmail.com>
2021-05-04 19:52:10 +0530

    added .env file

---
+++ src/firebase/config.js
@@ -0,0 +1,31 @@
+// import * as firebase from 'firebase/app' [OUTDATED] [doesn't work with latest firebase version]
+
+import firebase from 'firebase/app'
+import 'firebase/storage'
+import 'firebase/firestore'
+
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+}
+
+console.log(process.env.apiKey)
+
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+// a function to create timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
